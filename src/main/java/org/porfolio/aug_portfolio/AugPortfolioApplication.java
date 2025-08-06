28224bfe256d0b8fa2eb80646855446b8981ffda
tray0019 <tray0019@algonquinlive.com>
2025-08-06 16:48:20 -0400

    Set up my portfolio homepage using css bootstrap and thymleaf

---
+++ src/main/java/org/porfolio/aug_portfolio/AugPortfolioApplication.java
@@ -0,0 +1,13 @@
+package org.porfolio.aug_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AugPortfolioApplication.class, args);
+    }
+
+}
