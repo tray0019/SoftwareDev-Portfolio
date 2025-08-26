7127fb52ce6ebbf4098cba78db325e97f3376074
tray0019 <tray0019@algonquinlive.com>
2025-08-26 06:07:52 -0400

    updated and added resume page where i have written my technical skills and resume to be downloaded.

--- src/main/java/org/porfolio/aug_portfolio/HomeController.java
+++ src/main/java/org/porfolio/aug_portfolio/HomeController.java
@@ -1,7 +1,14 @@
 package org.porfolio.aug_portfolio;
 
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -12,4 +19,21 @@ public class HomeController {
         return "index";
     }
 
+    @GetMapping("/resume")
+    public String resumePage(){
+        return "resume";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/files/resume")
+        public ResponseEntity<Resource> downloadResume() throws Exception{
+            Resource pdf = new ClassPathResource("static/assets/Rustom_Trayvilla_Resume.pdf");
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION,
+                            "attachment; filename=\"Rustom_Trayvilla_Resume.pdf\"")
+                            .contentType(MediaType.APPLICATION_PDF)
+                    .contentLength(pdf.contentLength())
+                    .body(pdf);
+
+        }
 }
